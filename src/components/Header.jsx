7aa145ed56cd903497b1c19420e31f8a6e1d17aa
priyanshukumarsinha <1ds22cb039@dsce.edu.in>
2024-05-03 00:30:02 +0530

    feat/added more interactiveness to the auth button and added some animation :

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Logo from '../img/logo.png'
 
 import { MdShoppingBasket, MdAdd, MdLogout } from "react-icons/md";
@@ -21,14 +21,18 @@ const Header = () => {
   const userData = useSelector((state) => state.auth.user)
   const dispatch = useDispatch()
 
+  const [isMenu, setIsMenu] = useState(false);
+
   const adminEmail = "1ds22cb039@dsce.edu.in"
 
   const login = async () => {
+    setIsMenu(!isMenu);
     if (!userData) {
       const response = await signInWithPopup(auth, provider);
       const userData = response.user.providerData[0]
       dispatch(userLogin(userData));
       localStorage.setItem('user', JSON.stringify(userData))
+      setIsMenu(false)
     }
   }
 
@@ -52,12 +56,16 @@ const Header = () => {
         </Link>
 
         <div className=' w-full flex items-center justify-center gap-8'>
-          <ul className='flex items-center gap-10 ml-auto'>
+          <motion.ul 
+          initial = {{opacity : 0, x: 200}}
+          animate = {{opacity : 1, x: 0}}
+          exit = {{opacity : 0, x: 200}}
+          className='flex items-center gap-10 ml-auto'>
             <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer'>Home</li>
             <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer'>Menu</li>
             <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer'>About Us</li>
             <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer'>Service</li>
-          </ul>
+          </motion.ul>
           <div className="flex items-center relative">
             <MdShoppingBasket className='text-textColor text-2xl cursor-pointer' />
             <div className='w-5 h-5 rounded-full bg-red-500  ml-8 flex items-center justify-center absolute -top-2 -right-2'>
@@ -71,22 +79,38 @@ const Header = () => {
             <motion.img whileTap={{ scale: 0.6 }} src={userData ? userData.photoURL : Avatar} alt="userprofile" className='w-10 min-w-[40px] min-h-[40px] drop-shadow-xl cursor-pointer rounded-full'
               onClick={login}
             />
-            <div className='w-40 bg-gray-50 shadow-xl rounded-lg absolute top-12 right-0 flex flex-col'>
-              {
-                userData && userData.email === adminEmail && (
-                  <Link to={'/createItem'}>
+
+            {/* // show this only if img is clicked : done using state */}
+
+            {
+              isMenu && (
+
+                <motion.div 
+                initial = {{opacity: 0, scale : 0.6}}
+                animate = {{opacity: 1, scale : 1}}
+                exit = {{opacity: 0, scale : 0.6}}
+                className='w-40 bg-gray-50 shadow-xl rounded-lg absolute top-12 right-0 flex flex-col'>
+                  {
+                    userData && userData.email === adminEmail && (
+                      <Link to={'/createItem'}>
+                        <p className='px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-base text-textColor'
+                        >New Item
+                          <MdAdd />
+                        </p>
+                      </Link>
+                    )}
+                  {userData &&
                     <p className='px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-base text-textColor'
-                    >New Item
-                      <MdAdd />
+                      onClick={logout}
+                    >Logout
+                      <MdLogout />
                     </p>
-                  </Link>
-                )}
-              <p className='px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-base text-textColor'
-                onClick={logout}
-              >Logout
-                <MdLogout />
-              </p>
-            </div>
+                  }
+                </motion.div>
+              )
+            }
+
+
           </div>
 
 
