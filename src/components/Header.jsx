62c5a2bdb8edea75b4b3df2bf0f80d84c8c9ce7f
priyanshukumarsinha <1ds22cb039@dsce.edu.in>
2024-05-03 01:12:30 +0530

    Mobile Navbar Done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -37,6 +37,7 @@ const Header = () => {
   }
 
   const logout = () => {
+    setIsMenu(false)
     dispatch(userLogout())
     localStorage.clear();
   }
@@ -45,9 +46,10 @@ const Header = () => {
     Navigate('/createItem')
   )
 
+  const navItems = ["Home", "Menu", "About Us", "Service"];
 
   return (
-    <header className=' w-full p-6 px-16'>
+    <header className=' w-full p-3 px-4 md:p-6 md:px-16'>
       {/* Desktop & Tablet */}
       <div className='hidden md:flex p-4'>
         <Link to={'/'} className='flex items-center gap-8'>
@@ -56,15 +58,14 @@ const Header = () => {
         </Link>
 
         <div className=' w-full flex items-center justify-center gap-8'>
-          <motion.ul 
-          initial = {{opacity : 0, x: 200}}
-          animate = {{opacity : 1, x: 0}}
-          exit = {{opacity : 0, x: 200}}
-          className='flex items-center gap-10 ml-auto'>
-            <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer'>Home</li>
-            <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer'>Menu</li>
-            <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer'>About Us</li>
-            <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer'>Service</li>
+          <motion.ul
+            initial={{ opacity: 0, x: 200 }}
+            animate={{ opacity: 1, x: 0 }}
+            exit={{ opacity: 0, x: 200 }}
+            className='flex items-center gap-10 ml-auto'>
+
+            {navItems.map((item) => (<li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer'>{item}</li>))}
+
           </motion.ul>
           <div className="flex items-center relative">
             <MdShoppingBasket className='text-textColor text-2xl cursor-pointer' />
@@ -85,11 +86,11 @@ const Header = () => {
             {
               isMenu && (
 
-                <motion.div 
-                initial = {{opacity: 0, scale : 0.6}}
-                animate = {{opacity: 1, scale : 1}}
-                exit = {{opacity: 0, scale : 0.6}}
-                className='w-40 bg-gray-50 shadow-xl rounded-lg absolute top-12 right-0 flex flex-col'>
+                <motion.div
+                  initial={{ opacity: 0, scale: 0.6 }}
+                  animate={{ opacity: 1, scale: 1 }}
+                  exit={{ opacity: 0, scale: 0.6 }}
+                  className='w-40 bg-gray-50 shadow-xl rounded-lg absolute top-12 right-0 flex flex-col'>
                   {
                     userData && userData.email === adminEmail && (
                       <Link to={'/createItem'}>
@@ -119,8 +120,62 @@ const Header = () => {
 
       </div>
       {/* Mobile */}
-      <div className='flex md:hidden bg-blue-600 p-4'>
+      <div className='flex md:hidden p-4 justify-between items-center'>
+        <div className="flex items-center relative">
+          <MdShoppingBasket className='text-textColor text-2xl cursor-pointer' />
+          <div className='w-5 h-5 rounded-full bg-red-500  ml-8 flex items-center justify-center absolute -top-2 -right-2'>
+            <p className='text-xs text-white  font-semibold'>
+              2
+            </p>
+          </div>
+        </div>
+        
+        <Link to={'/'} className='flex items-center gap-8'>
+          <img src={Logo} className='w-8 object-cover' alt="logo" />
+          <p className='text-headingColor text-xl font-bold'>City</p>
+        </Link>
+        
 
+        <div className="relative">
+          <motion.img whileTap={{ scale: 0.6 }} src={userData ? userData.photoURL : Avatar} alt="userprofile" className='w-10 min-w-[40px] min-h-[40px] drop-shadow-xl cursor-pointer rounded-full'
+            onClick={login}
+          />
+
+          {
+            isMenu && userData && (
+
+              <motion.div
+                initial={{ opacity: 0, scale: 0.6 }}
+                animate={{ opacity: 1, scale: 1 }}
+                exit={{ opacity: 0, scale: 0.6 }}
+                className='w-40 bg-gray-50 shadow-xl rounded-lg absolute top-12 right-0 flex flex-col'>
+                {
+                  userData && userData.email === adminEmail && (
+                    <Link to={'/createItem'}>
+                      <p className='px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-base text-textColor'
+                      >New Item
+                        <MdAdd />
+                      </p>
+                    </Link>
+                  )}
+
+                <ul className='flex flex-col'>
+
+                  {navItems.map((item) => (<li className='text-base text-textColor hover:bg-slate-100 transition-all duration-100 ease-in-out cursor-pointer px-4 py-2' >{item}</li>))}
+
+                </ul>
+
+                {userData &&
+                  <p className='m-2 p-2 rounded-mg shadow-md flex items-center justify-center bg-gray-200 gap-3 cursor-pointer hover:bg-gray-300 transition-all duration-100 ease-in-out text-base text-textColor'
+                    onClick={logout}
+                  >Logout
+                    <MdLogout />
+                  </p>
+                }
+              </motion.div>
+            )
+          }
+        </div>
       </div>
     </header>
   )
