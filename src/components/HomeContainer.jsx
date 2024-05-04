55608ebd44ff5d85f8f15297ace45aab9b07c6dc
priyanshukumarsinha <1ds22cb039@dsce.edu.in>
2024-05-04 08:16:26 +0530

    Home Component Done

--- src/components/HomeContainer.jsx
+++ src/components/HomeContainer.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Delivery from '../img/delivery.png'
 import HeroBg from '../img/heroBg.png'
+import heroData from '../utils/data'
 
 const HomeContainer = () => {
   return (
@@ -14,8 +15,8 @@ const HomeContainer = () => {
             <img src={Delivery} alt="delivery" className='w-full h-full object-contain ' />
           </div>
         </div>
-        <p className='text-[3rem] lg:text-[4rem] md:text-[3.5rem] font-bold tracking-wide text-headingColor '>
-          The Fastest Delivery in <span className='text-orange-600 text-[3.5rem] lg:text-[5rem] md:text-[4.5rem] '> Your City</span>
+        <p className='text-[2.5rem] lg:text-[4rem] md:text-[3rem] font-bold tracking-wide text-headingColor '>
+          The Fastest Delivery in <span className='text-orange-600 text-[3rem] lg:text-[5rem] md:text-[3.5rem] '> Your City</span>
         </p>
 
         <p className='text-base text-textColor text-center md:w-[80%] md:text-left'>
@@ -25,10 +26,26 @@ const HomeContainer = () => {
         <button type='button' className='bg-gradient-to-br from-orange-400 to-orange-500 w-full px-4 py-2 rounded-lg hover:shadow-lg transition-all duration-100 ease-in-out md:w-auto'>Order Now</button>
 
       </div>
-      <div className='py-2 flex-1' >
-        <img src={HeroBg} className = "lg:h-650 h-420 w-full lg:w-auto ml-auto" alt="" />
-        <div className="w-full h-full absolute items-center justify center">
-
+      <div className='py-2 flex-1 relative' >
+        <img src={HeroBg} className = "lg:h-650 h-420 w-full lg:w-auto ml-auto" alt="herobg" />
+        <div className="w-full h-full absolute top-0 left-0 flex items-center justify-center py-4 xl:px-32 gap-4 flex-wrap drop-shadow-lg">
+           {
+            heroData && (
+            heroData.map((data) => (
+                <div key= {data.id} className="lg:w-190  p-4 bg-cardOverlay backdrop-blur-md rounded-3xl flex items-center justify-center flex-col">
+                    <img src={data.imgSrc} alt="i1" className='-mt-10 lg:-mt-20 w-20 lg:w-40'/>
+                    <p className='text-base lg:text-xl font-semibold text-textColor mt-2 lg:mt-4'>
+                        {data.name}
+                    </p>
+                    <p className='text-[12px] lg:text-sm text-lighttextGray font-semibold my-1 lg:my-3'>
+                        {data.desc}
+                    </p>
+                    <p className='text-sm font-semibold text-headingColor'>
+                        <span className='text-xs text-red-600'>$</span> {data.price}
+                    </p>
+                </div>
+            )))
+           }
         </div>
       </div>
     </section>
